818e9bc9ca050bdacce2f8f36a0fcfaef17c8e80
unknown <leroyfenix@gmail.com>
2026-01-14 23:46:25 +0800

    Add Render domain to ALLOWED_HOSTS

--- payatas/settings.py
+++ payatas/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$al_#ecrh7(r80eza@z^67jsigx^%nnt-51csw4v)+s$%)h!)5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['football-database-tracker-v2.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -129,6 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
